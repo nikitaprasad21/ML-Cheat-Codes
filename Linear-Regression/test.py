0234e3b381a5178a89858cfc9a4691ad694cef86
Nikita Prasad <84131752+nikitaprasad21@users.noreply.github.com>
2024-01-19 13:49:39 +0530

    Update test.txt

--- Linear-Regression/test.py
+++ Linear-Regression/test.py
@@ -1,1 +1,12 @@
-jvj 
+3 Dataset url
+acme_url = "https://raw.githubusercontent.com/JovianML/opendatasets/master/data/medical-charges.csv"
+# Importing url retrieve method from urllib.request 
+from urllib.request import urlretrieve
+
+urlretrieve(acme_url, "acme_dataset.csv")
+('acme_dataset.csv', <http.client.HTTPMessage at 0x217c6eeb850>)
+# Now creating a Pandas dataframe using the downloaded file
+import pandas as pd
+
+acme_data = pd.read_csv("acme_dataset.csv")
+acme_data
